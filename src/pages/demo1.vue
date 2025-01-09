d8845abf3b6ae2094e5ae7fa4c88eecd70077d1f
jiangmenghang <e912935521@163.com>
2025-01-09 13:57:09 +0800

    edit demo

--- src/pages/demo1.vue
+++ src/pages/demo1.vue
@@ -1,7 +1,7 @@
 <template>
   <button @click="addParagraph">新增一段话</button>
   <button @click="getText">获取纯文本</button>
-  <button @click="demoChangeName('张三六六六')">名字变成张三</button>
+  <button @click="demoChangeName('张三')">名字变成张三</button>
   <button @click="demoChangeName('')">名字自定义</button>
   <div style="width: 300px; height: 200px; margin: 40px">
     <AiInputEditor ref="editorRef" @select-the-tag="onSelectTag"></AiInputEditor>
@@ -15,7 +15,7 @@ import AiInputEditor, { createSelectTag } from '../lib/ai-input-tiptap'
 const editorRef = ref<InstanceType<typeof AiInputEditor> | null>(null)
 
 const addParagraph = () => {
-  const nameTag = createSelectTag({ tagKey: 'name', placeholder: '[患者姓名]', content: '张三' })
+  const nameTag = createSelectTag({ tagKey: 'name', placeholder: '[患者姓名]', content: '王六' })
   const symptomTag = createSelectTag({
     tagKey: 'symptom',
     placeholder: '[症状]',
@@ -31,9 +31,9 @@ const getText = () => {
 }
 
 const demoChangeName = (content: string) => {
-  const nameTag = createSelectTag({ tagKey: 'name2', placeholder: '[患者姓名]', content })
+  const nameTag = createSelectTag({ tagKey: 'name', placeholder: '[患者姓名]', content })
   const nameTagNotFound = `，患者的标签特征有${nameTag}`
-  editorRef.value?.editor?.commands.updateSelectTagByKey('name2', nameTag, nameTagNotFound)
+  editorRef.value?.editor?.commands.updateSelectTagByKey('name', nameTag, nameTagNotFound)
 }
 
 const onSelectTag = () => {}
